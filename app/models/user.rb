c64726a06c0d2605708f4f59fd2e9a07177555d5
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-01-03 00:18:31 +0800

    implement is_member_of?(group)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :group_relationships
   has_many :participated_groups, :through => :group_relationships, :source => :group
+
+  def is_member_of?(group)
+    participated_groups.include?(group)
+  end
 end
