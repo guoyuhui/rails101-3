be1a89579a9711f0cb3c63ce7d55b40be5fd0676
aliceguo2016reborn <aliceguo2016reborn@gmail.com>
2017-01-03 00:34:30 +0800

    join&quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -46,6 +46,29 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+     if !current_user.is_member_of?(@group)
+       current_user.join!(@group)
+       flash[:notice] = "加入本讨论版成功"
+     else
+       flash[:warning] = "你已经是本讨论版成员"
+     end
+     redirect_to group_path(@group)
+   end
+
+   def quit
+     @group = Group.find(params[:id])
+
+     if current_user.is_member_of?(@group)
+       current_user.quit!(@group)
+       flash[:alert] = "已退出本讨论版"
+     else
+       flash[:warning] = "你不是本讨论版成员，怎么退出"
+     end
+   end
+
   private
 
   def find_group_and_check_permission
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
 
 resources :groups do
+  member do
+    post :join
+    post :quit
+  end
+  
   resources :posts
 end
 root "groups#index"
